1e1b7859e77ee5d7d105929a808ea0886ad9a87a
AbhiDeb <abhitesh.ait@gmail.com>
2017-01-30 22:40:23 +0530

    Feature Scaling in Python

--- DataPreprocessing/DataPreprocessing.py
+++ DataPreprocessing/DataPreprocessing.py
@@ -69,3 +69,12 @@ Y = labelencoder_Y.fit_transform(Y)
 from sklearn.cross_validation import train_test_split
 #Now we will make 4 matrix for our 2 splitted datasets
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.2,random_state=0)
+
+#==============================================================================
+#Feature Scaling
+#==============================================================================
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+#Since we already fit_transform sc_X, we will only use transform for next use
+X_test = sc_X.transform(X_test)
